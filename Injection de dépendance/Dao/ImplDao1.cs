7b53f66e915407d5413c0dd452557a7fb5bb7e39
samba abd allah sy <sambasy20@hotmail.com>
2019-01-26 15:27:26 +0100

    Creer la partie Dao et Metier avec Injection de dépendance

---
+++ Injection de dépendance/Dao/ImplDao1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injection_de_dépendance.Dao
+{
+    public class ImplDao1 : IDao
+    {
+        public int Calcul()
+        {
+            return 45;
+        }
+    }
+}
---
+++ Injection de dépendance/Metier/ImplMetier.cs
@@ -0,0 +1,22 @@
+﻿using Injection_de_dépendance.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injection_de_dépendance.Metier
+{
+    class ImplMetier : IMetier
+    {
+        private IDao _dao;
+        public ImplMetier(IDao dao)
+        {
+            this._dao = dao;
+        }
+        public int Calcul()
+        {
+            return _dao.Calcul() * 3;
+        }
+    }
+}
